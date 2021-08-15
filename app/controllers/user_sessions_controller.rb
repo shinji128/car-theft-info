aa8b46ad1331f5190fd819b1142734ac3d2783be
shinji256 <shinji65536@gmail.com>
2021-08-15 17:32:42 +0900

    Add_user

---
+++ app/controllers/user_sessions_controller.rb
@@ -0,0 +1,23 @@
+class UserSessionsController < ApplicationController
+  skip_before_action :require_login, only: %i[new create]
+
+  def new; end
+
+  def create
+    @user = login(params[:email], params[:password])
+    if @user
+      # redirect_back_or_to new_post_path, success: t('.success')
+      redirect_to root_path
+    else
+      # flash.now[:danger] = t('.fail')
+      flash.now[:danger] = 'ログインできません'
+      render :new
+    end
+  end
+
+  def destroy
+    logout
+    # redirect_to post_path, success: t('.success')
+    redirect_to root_path
+  end
+end
