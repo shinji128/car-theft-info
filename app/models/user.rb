274095489be9e146a7afa3e7f9ac7cf394e728eb
shinji256 <shinji65536@gmail.com>
2021-08-15 19:08:59 +0900

    rubocopに従いuserモデル修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
 
   validates :password, length: { minimum: 7 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
-  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
+  validates :password_confirmation, presence: true,
+                                    if: -> { new_record? || changes[:crypted_password] }
 
 end
