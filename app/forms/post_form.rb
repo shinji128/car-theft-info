71ddb45a4f9246fdc826a4ced381a779d3441965
shinji256 <shinji65536@gmail.com>
2021-09-25 21:49:43 +0900

    盗難記事のバリデーションを追加

--- app/forms/post_form.rb
+++ app/forms/post_form.rb
@@ -12,16 +12,25 @@ class PostForm
   attribute :user_id, :integer
   attribute :post
 
+  validates :car_name, presence: true, length: { maximum: 50 }
+  validates :car_number, presence: true, length: { maximum: 50 }
+  validates :stole_location, presence: true
+  validates :images, presence: true
+
+  validate :check_image
+  validate :image_count
+
   def save
     return false if invalid?
 
-    self.post = Post.new(post_params) # newがcreateじゃね？
-    post.save
+    ActiveRecord::Base.transaction do
+      self.post = Post.new(post_params) # newがcreateじゃね？
+      post.save
 
-    images.each do |image|
-      post.images.create!(image: image)
+      images.each do |image|
+        post.images.create!(image: image)
+      end
     end
-
     true
   end
 
@@ -37,5 +46,24 @@ class PostForm
       stole_time: stole_time,
       user_id: user_id
     }
+  end
+
+  def check_image
+    return false if images.blank?
+
+    extension_allowlist = %w[image/jpg image/jpeg image/png]
+    images&.each do |image|
+      if !extension_allowlist.include?(image.content_type)
+        errors.add(:images, 'は jpg/jpeg/png のみアップロードできます')
+      elsif image.size > 3.megabyte
+        errors.add(:images, '3MBまでアップロードできます')
+      end
+    end
+  end
+
+  def image_count
+    return false if images.blank?
+
+    errors.add('画像は5枚までです') if images.count >= 6
   end
 end
