61ec3337ca290518dde1343aff24254fe8ff85b2
shinji256 <shinji65536@gmail.com>
2021-10-04 00:20:08 +0900

    不要な記述を削除

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,11 +2,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("swiper")
 require("swiper/swiper-bundle.js")
 
-require('../packs/swiper')
-require('../packs/test')
 import '../stylesheets/application';
 
 Rails.start()
