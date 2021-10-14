cd4f2c1ee585ee4c29221ba19bed6d62d00149e3
shinji256 <shinji65536@gmail.com>
2021-10-14 19:54:38 +0900

    swiper修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,5 @@
 require("@rails/ujs").start()
-require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("swiper/swiper-bundle.js")
 
 import '../stylesheets/application';
-
-Rails.start()
